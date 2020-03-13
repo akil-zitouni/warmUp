603ecfae35df711a8eacf6863572c1fe1c0921f3
akil-zitouni <akil.akil12@live.fr>
2020-03-13 10:46:14 +0100

    this is it

--- warmUp1.js
+++ warmUp1.js
@@ -1,5 +1,15 @@
-// 1-using + operator combine your partner first and last name .
+// 1-using + operator combine your partner first and last name 
+function partnername (firstname,lastname) {
+	return firstname + ' ' + lastname	
+}
 // 2-find if the number 13 is a multiple of 3 or not.
+function multiple(n){
+     if (n % 13 === 0) {
+     	return ('n is a multiple of 13');
+     }
+     else
+     	return ('n is not a multiple of 13');
+}
 // 3-calculate the average age of the follwing ages [13,14,13,15,16,17,19,13,16,15]
 // 4-calculate your age in seconds.
 
